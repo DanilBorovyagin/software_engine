90e209e74ea44d3c2b206e8a64eab41ecd55badc
DanilBorovyagin <Dan.los.2019@mail.ru>
2023-09-23 17:33:57 +0300

    удаление из новой ветки

--- lab_3_py/lab_3.7.py
+++
@@ -1,4 +0,0 @@
-val = 100
-for i in range(10, -1, -2):
-    val -= i
-    print(i,val)
--- lab_3_py/lab_3.9.py
+++
@@ -1,9 +0,0 @@
-val = 0
-for i in range(3):
-    for j in range(3):
-        if i != j:
-             val += j
-        else:
-            pass
-
-print(val)
